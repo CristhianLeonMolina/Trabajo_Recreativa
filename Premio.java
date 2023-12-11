be279014856401322e38d927377547c5f91cbf5e
victormateo11 <152964275+victormateo11@users.noreply.github.com>
2023-12-11 09:16:29 +0100

    clase premio

---
+++ Premio.java
@@ -0,0 +1,36 @@
+package JuegosRecreativos;
+
+public class Premio {
+    private String nombre;
+    private int coste, stock;
+    
+    Premio(String nombre, int coste, int stock){
+        this.setNombre(nombre);
+        this.setCoste(coste);
+        this.setStock(stock);
+    }
+    
+    public String getNombre() {
+    return this.nombre;
+    }
+    
+    public int getCoste() {
+    return this.coste;
+    }
+    
+    public int getStock() {
+    return this.stock;
+    }
+    
+    public void setNombre(String nombre) {
+    this.nombre = nombre;
+    }
+    
+    public void setCoste(int coste) {
+    this.coste = coste;
+    }
+    
+    public void setStock(int stock) {
+    this.stock = stock;
+    }
+}
