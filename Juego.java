d30fdedf8adc4db127bac6392f4993e31986d8c5
S <146083097+saragar2@users.noreply.github.com>
2023-12-12 10:44:58 +0100

    Juego y Tarjeta terminados?

---
+++ Juego.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recreativos;
+
+/**
+ *
+ * @author developer
+ */
+public class Juego
+{
+    private String  nombre;
+    private int     coste;
+    
+    //constructor para la clase
+    public Juego(String nombre, int coste)
+    {
+        this.setNombre(nombre);
+        this.setCoste(coste);
+    }
+    
+    //getter para obtener el nombre del juego al que nos referimos
+    public String getNombre()
+    {
+        return (this.nombre);
+    }
+    
+    //getter para obtener el precio del juego al que nos referimos
+    public int getCoste()
+    {
+        return (this.coste);
+    }
+    
+    //setter para nombrar o modificar el nombre del jeugo
+    private void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    //setter para dar valor o modificar el valor del precio del juego
+    private void setCoste(int coste)
+    {
+        if (coste >= 0)
+            this.coste = coste;
+        else
+            System.out.println("Parámetro no válido: coste");
+    }
+    
+    //metodo para pagar el juego al que se quiere jugar, restando los créditos y sumando los nuevos tickets que han sido generados aleatoriamente
+    public void pagar(Tarjeta tarjetaPago)
+    {
+        if ((tarjetaPago.getCreditos() - this.coste) >= 0)
+        {
+            tarjetaPago.setCreditos(tarjetaPago.getCreditos() - this.coste);
+            tarjetaPago.setTickets(tarjetaPago.getTickets() + (int)(Math.random()*51));
+        }
+        else
+            System.out.println("Saldo insuficiente");
+    }
+    
+    //metodo que pasa a string el nombre del juego y su precio
+    @Override
+    public String toString()
+    {
+        return ("El juego " + this.nombre + " tiene un precio de " + this.coste + " creditos");
+    }
+    
+}
--- Tarjeta.java
+++ Tarjeta.java
@@ -16,46 +16,54 @@ public class Tarjeta
     private int numTarjeta;
     private int i = 0;
     
-    public Tarjeta(int creditos, int tickets)
+    //constructor de la clase
+    public Tarjeta(int creditos)
     {
         this.setCreditos(creditos);
         this.setTickets(0);
         this.setNumTarjeta(i++);
     }
     
+    //getter para obtener la cantidad de creditos en la tarjeta
     public int getCreditos()
     {
         return (this.creditos);
     }
     
+    //getter para obtener la cantidad de tickets que posee el usuario almacenados en la tarjeta
     public int getTickets()
     {
         return (this.tickets);
     }
     
+    //getter para obtener el numero de la tarjeta del usuario
     public int getNumTarjeta()
     {
         return (this.numTarjeta);
     }
     
+    //setter para cambiar la cantidad de creditos en la tarjeta
     public void setCreditos(int creditos)
     {
-        if (creditos >= 0)
-            this.creditos = creditos;
-        else
-            System.out.println("Parámetro no válido: creditos");
+        this.creditos = creditos;
     }
     
+    //setter para cambiar la cantidad de tickets en la tarjeta
     public void setTickets(int tickets)
     {
-        if (tickets >= 0)
-            this.tickets = tickets;
-        else
-            System.out.println("Parámetro no válido: tickets");
+        this.tickets = tickets;
     }
     
+    //setter para establecer el número de la nueva tarjeta
     public void setNumTarjeta(int numTarjeta)
     {
         this.numTarjeta = numTarjeta;
+    }
+    
+    //metodo toString que pasa a string el numero de la tarjeta, la cantidad de creditos y la cantidad de tickets
+    @Override
+    public String toString()
+    {
+        return ("La tarjeta " + this.numTarjeta + " cuenta con " + this.creditos + " y tiene " + this.tickets + " tickets.");
     }
 }
