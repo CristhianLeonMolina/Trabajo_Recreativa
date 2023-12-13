63761108082e948a93081c35133d71c2c54cb5b2
victormateo11 <152964275+victormateo11@users.noreply.github.com>
2023-12-13 13:59:39 +0100

    puta francia

--- Premio.java
+++ Premio.java
@@ -5,29 +5,29 @@ public class Premio {
     private String nombre;
     private int coste, stock;
 
-    public Premio(String nombre, int coste, int stock) {
+    public Premio(String nombre, int coste, int stock) { //Constructor de la clase "Premio" con los atributos nombre, coste y stock.
         this.setNombre(nombre);
         this.setCoste(coste);
         this.setStock(stock);
     }
 
-    public String getNombre() {
+    public String getNombre() { //Método getter del atributo nombre.
         return this.nombre;
     }
 
-    public int getCoste() {
+    public int getCoste() { //Método getter del atributo coste.
         return this.coste;
     }
 
-    public int getStock() {
+    public int getStock() { //Método getter del atributo stock.
         return this.stock;
     }
 
-    public void setNombre(String nombre) {
+    public void setNombre(String nombre) { //Método setter del atributo nombre.
         this.nombre = nombre;
     }
 
-    public void setCoste(int coste) {
+    public void setCoste(int coste) { //Método setter del atributo coste.
         if (coste < 0) {
             System.out.println("En este establecimiento no disponemos de productos gratuitos");
         } else {
@@ -35,7 +35,7 @@ public class Premio {
         }
     }
 
-    public void setStock(int stock) {
+    public void setStock(int stock) { //Método setter del atributo stock.
         if (stock < 0) {
             System.out.println("No podemos tener un stock menor que 0 unidades");
         } else {
@@ -44,7 +44,7 @@ public class Premio {
     }
     
     @Override
-    public String toString() {
+    public String toString() { //Paso a String de los atributos del premio.
     return ("Nos quedan " + this.getStock() + " " + this.getNombre().toLowerCase() + " con un coste de " + this.getCoste() + " tickets.");
     }
 }
--- Terminal.java
+++ Terminal.java
@@ -1,81 +1,81 @@
 package JuegosRecreativos;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Terminal {
 
     private Premio NombrePremios[] = new Premio[3];
 
-    public Terminal(Premio premio1, Premio premio2, Premio premio3) {
+    public Terminal(Premio premio1, Premio premio2, Premio premio3) { //Constructor de la clase "Terminal" que permite almacenar en al array 3 objetos de la clase "Premio".
         this.setNombrePremios(0, premio1);
         this.setNombrePremios(1, premio2);
         this.setNombrePremios(2, premio3);
     }
 
-    public void setNombrePremios(int posicionPremio, Premio premio) {
+    public void setNombrePremios(int posicionPremio, Premio premio) { //Método setter que selecciona un premio y lo guarda en una posición exacta del array "NombrePremios".
         this.NombrePremios[posicionPremio] = premio;
     }
 
-    public Premio[] getNombrePremios() {
+    public Premio[] getNombrePremios() { //Método getter que devuelve el array "NombrePremios".
         return this.NombrePremios;
     }
 
-    public void convertir(Tarjeta tarjeta, int dinero) {
+    public void convertir(Tarjeta tarjeta, int dinero) { //Método para convertir dinero en tickets.
         if (dinero >= 0) {
             tarjeta.setTickets(tarjeta.getTickets() + (dinero * 2));
             System.out.println(tarjeta.toString());
         } else {
-            System.out.println("Error");
+            System.out.println("Has introducido un valor negativo");
         }
     }
 
-    public void consultar(Tarjeta tarjeta) {
+    public void consultar(Tarjeta tarjeta) { //Método que consulta los datos de la tarjeta.
         System.out.println(tarjeta.toString());
     }
 
-    public void transferir(Tarjeta tarjeta1, Tarjeta tarjeta2) {
+    public void transferir(Tarjeta tarjeta1, Tarjeta tarjeta2) { //Método que permite transferir créditos o tickets de una tarjeta a otra.
         Scanner sc = new Scanner(System.in);
         int valorTransferencia = 0;
         int eleccion = 0;
-        int cont = 1;
-        for (int i = 0; i < cont; i++) {
-            System.out.println("Introduce un número acorde a la transferencia" + "\n"
+        try {
+        do {
+            System.out.println("Introduce un número acorde al tipo de transferencia" + "\n"
                     + "0. Tickets" + "\n"
                     + "1. Créditos" + "\n");
             eleccion = sc.nextInt();
-            if (eleccion != 0 && eleccion != 1) {
-                cont++;
-            }
+        } while (eleccion != 0 && eleccion != 1);
+        } catch (InputMismatchException ex) {
+            System.out.println("Carácter no permitido");
         }
         int contadorEleccion = 1;
         if (eleccion == 0) {
             for (int i = 0; i < contadorEleccion; i++) {
-                System.out.println("");
-                System.out.print("Introduce un valor a transferir de tickets: ");
+                System.out.print("Introduce los tickets a transferir: ");
                 valorTransferencia = sc.nextInt();
                 if (valorTransferencia < 0) {
-                    System.out.println("Error");
+                    System.out.println("Has introducido un valor negativo, inténtalo de nuevo.");
                     contadorEleccion++;
                 } else if (valorTransferencia <= tarjeta1.getTickets()) {
                     tarjeta1.setTickets(tarjeta1.getTickets() - valorTransferencia);
                     tarjeta2.setTickets(tarjeta2.getTickets() + valorTransferencia);
                 } else {
-                    System.out.println("Has intentado transferir más tickets de los que tienes");
+                    System.out.println("Has intentado transferir más tickets de los que tienes, inténtalo de nuevo.");
                     contadorEleccion++;
                 }
             }
         } else {
             for (int i = 0; i < contadorEleccion; i++) {
-                System.out.print("Introduce un valor a transferir de créditos: ");
+                System.out.print("Introduce los créditos a transferir: ");
                 valorTransferencia = sc.nextInt();
                 if (valorTransferencia < 0) {
-                    System.out.println("Error");
+                    System.out.println("Has introducido un valor negativo, inténtalo de nuevo.");
                     contadorEleccion++;
                 } else if (valorTransferencia <= tarjeta1.getCreditos()) {
                     tarjeta1.setCreditos(tarjeta1.getCreditos() - valorTransferencia);
                     tarjeta2.setCreditos(tarjeta2.getCreditos() + valorTransferencia);
                 } else {
-                    System.out.println("Has intentado transferir más créditos de los que tienes");
+                    System.out.println("Has intentado transferir más créditos de los que tienes, inténtalo de nuevo.");
                     contadorEleccion++;
                 }
             }
@@ -83,7 +83,7 @@ public class Terminal {
         System.out.println(toString());
     }
 
-    public void canjear(Tarjeta tarjeta, Premio premio) {
+    public void canjear(Tarjeta tarjeta, Premio premio) { //Método que permite canjear tickets por premios.
         int tenerPremio = 0;
         for (int i = 0; i < NombrePremios.length; i++) {
             if (premio == NombrePremios[i]) {
@@ -92,30 +92,28 @@ public class Terminal {
                 if (premio.getStock() > 0) {
                     tenerPremio++;
                     if (premio.getCoste() > tarjeta.getTickets()) {
-                        System.out.println("El coste del premio es mayor que los tickets que dispones en la tarjeta");
+                        System.out.println("No te puedes permitir este premio, consigue más tickets 😀");
                     } else {
                         tenerPremio++;
                     }
                 }
             } else {
-                System.out.println("No tenemos el premio en este terminal");
+                System.out.println("No disponemos del premio solicitado en este terminal");
             }
 
         }
         if (tenerPremio == 3) {
             premio.setStock(premio.getStock() - 1);
             System.out.println("Ya puedes recoger el premio: " + premio.getNombre());
-            System.out.println("Quedan " + premio.getStock() + " premios en este terminal");
+            System.out.println("Nos quedan " + premio.getStock() + " unidades de este premio en el terminal actual");
         }
     }
 
     @Override
-    public String toString() {
+    public String toString() { //Paso a String de los atributos de cada premio del array que contiene los premios disponibles.
         return ("En este terminal disponemos de los siguientes premios:" + "\n"
-                + this.NombrePremios[0].getNombre() + " , nos quedan " + this.NombrePremios[0].getStock() + " y su coste es de " + this.NombrePremios[0].getCoste() + " tickets" + "\n"
-                + this.NombrePremios[1].getNombre() + " , nos quedan " + this.NombrePremios[1].getStock() + " y su coste es de " + this.NombrePremios[1].getCoste() + " tickets" + "\n"
-                + this.NombrePremios[2].getNombre() + " , nos quedan " + this.NombrePremios[2].getStock() + " y su coste es de " + this.NombrePremios[2].getCoste() + " tickets");
+                + this.NombrePremios[0].getNombre() + ", nos quedan " + this.NombrePremios[0].getStock() + " y su coste es de " + this.NombrePremios[0].getCoste() + " tickets" + "\n"
+                + this.NombrePremios[1].getNombre() + ", nos quedan " + this.NombrePremios[1].getStock() + " y su coste es de " + this.NombrePremios[1].getCoste() + " tickets" + "\n"
+                + this.NombrePremios[2].getNombre() + ", nos quedan " + this.NombrePremios[2].getStock() + " y su coste es de " + this.NombrePremios[2].getCoste() + " tickets" + "\n");
     }
-    //cambiar bucles for por while
 }
-//revisar sout
