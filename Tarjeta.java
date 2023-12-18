db988bf790dc774055556240bb39eb19d7831d40
S <146083097+saragar2@users.noreply.github.com>
2023-12-18 08:55:43 +0100

    comentarios adecuados para el javadoc en clase Tarjeta

--- Tarjeta.java
+++ Tarjeta.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package recreativos;
-
-/**
- *
- * @author developer
- */
 public class Tarjeta
 {
     private int creditos;
@@ -16,7 +6,7 @@ public class Tarjeta
     private int numTarjeta;
     static int i = 0;
     
-    //constructor de la clase
+    /**constructor de la clase*/
     public Tarjeta(int creditos)
     {
         this.setCreditos(creditos);
@@ -24,44 +14,37 @@ public class Tarjeta
         this.setNumTarjeta(i++);
     }
     
-    //getter para obtener la cantidad de creditos en la tarjeta
+    /**getter para obtener la cantidad de creditos en la tarjeta*/
     public int getCreditos()
     {
         return (this.creditos);
     }
     
-    //getter para obtener la cantidad de tickets que posee el usuario almacenados en la tarjeta
+    /**getter para obtener la cantidad de tickets que posee el usuario almacenados en la tarjeta*/
     public int getTickets()
     {
         return (this.tickets);
     }
     
-    //getter para obtener el numero de la tarjeta del usuario
+    /**getter para obtener el numero de la tarjeta del usuario*/
     public int getNumTarjeta()
     {
         return (this.numTarjeta);
     }
     
-    //setter para cambiar la cantidad de creditos en la tarjeta
+    /**setter para cambiar la cantidad de creditos en la tarjeta*/
     public void setCreditos(int creditos)
     {
         this.creditos = creditos;
     }
     
-    //setter para cambiar la cantidad de tickets en la tarjeta
+    /**setter para cambiar la cantidad de tickets en la tarjeta*/
     public void setTickets(int tickets)
     {
         this.tickets = tickets;
     }
     
-    //setter para establecer el número de la nueva tarjeta
-    //se puede quitar, ya que como el numero de tarjeta es estatico no necesita cambiarse
-    public void setNumTarjeta(int numTarjeta)
-    {
-        this.numTarjeta = numTarjeta;
-    }
-    
-    //metodo toString que pasa a string el numero de la tarjeta, la cantidad de creditos y la cantidad de tickets
+    /**metodo toString que pasa a string el numero de la tarjeta, la cantidad de creditos y la cantidad de tickets*/
     @Override
     public String toString()
     {
