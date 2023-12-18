3d4e76ea4e98a81508fb10f7b557ccaf2f05c005
S <146083097+saragar2@users.noreply.github.com>
2023-12-18 10:03:51 +0100

    he metido los param de mierda
    espero que nadie lea este commit porque la verdad es que estoy al borde de la locura, la cual solo se va a curar cuando alguien me haga un bizum de 1000€ o más asique por favor sed almas caritativas y aayudad a esta pobre programadora a hacer Java (perdon soy vim enjoyer)

--- Tarjeta.java
+++ Tarjeta.java
@@ -14,37 +14,51 @@ public class Tarjeta
         this.Tarjeta(i++);
     }
     
-    /**getter para obtener la cantidad de creditos en la tarjeta*/
+    /**getter para obtener la cantidad de creditos en la tarjeta
+    * @param créditos: Cantidad de créditos alamcenados en la tarjeta
+    */
     public int getCreditos()
     {
         return (this.creditos);
     }
     
-    /**getter para obtener la cantidad de tickets que posee el usuario almacenados en la tarjeta*/
+    /**getter para obtener la cantidad de tickets que posee el usuario almacenados en la tarjeta
+    * @param tickets: Cantidad de tickets alamcenados en la tarjeta
+    */
     public int getTickets()
     {
         return (this.tickets);
     }
     
-    /**getter para obtener el numero de la tarjeta del usuario*/
+    /**getter para obtener el numero de la tarjeta del usuario
+    * @param numTarjeta: Numero de identificación de la tarjeta
+    */
     public int getNumTarjeta()
     {
         return (this.numTarjeta);
     }
     
-    /**setter para cambiar la cantidad de creditos en la tarjeta*/
+    /**setter para cambiar la cantidad de creditos en la tarjeta
+    * @param créditos: Cantidad de créditos alamcenados en la tarjeta
+    */
     public void setCreditos(int creditos)
     {
         this.creditos = creditos;
     }
     
-    /**setter para cambiar la cantidad de tickets en la tarjeta*/
+    /**setter para cambiar la cantidad de tickets en la tarjeta
+    * @param tickets: Cantidad de tickets alamcenados en la tarjeta
+    */
     public void setTickets(int tickets)
     {
         this.tickets = tickets;
     }
     
-    /**metodo toString que pasa a string el numero de la tarjeta, la cantidad de creditos y la cantidad de tickets*/
+    /**metodo toString que pasa a string el numero de la tarjeta, la cantidad de creditos y la cantidad de ticketsç
+    * @param créditos: Cantidad de créditos alamcenados en la tarjeta
+    * tickets: Cantidad de tickets alamcenados en la tarjeta
+    * numTarjeta: Numero de identificación de la tarjeta
+    */
     @Override
     public String toString()
     {
